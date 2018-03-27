03c5be13b0b6460085774ab03cd6009d6be989aa
Marco Link <marcoxlink@googlemail.com>
2018-03-27 14:36:24 +0200

    Add delete/update functionality for entries on option page

--- src/js/options.jsx
+++ src/js/options.jsx
@@ -1,7 +1,7 @@
 import "../css/options.css";
 import * as ReactDOM from "react-dom";
 import React from "react";
-import {entries, removeEntry} from "./keyword_access";
+import {addEntry, entries, removeEntry} from "./keyword_access";
 import PropTypes from "prop-types";
 
 export class OptionsList extends React.Component {
@@ -10,10 +10,29 @@ export class OptionsList extends React.Component {
         super(props);
         this.state = {items: null};
         this.fetchItems();
+        this.bindFunctions();
+    }
+
+    bindFunctions() {
+        this.removeItem = this.removeItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+        this.changeItem = this.changeItem.bind(this);
     }
 
     fetchItems() {
-        entries().then((items) => this.setState({items: items}));
+        return entries().then((items) => this.setState({items: items}));
+    }
+
+    removeItem(item) {
+        removeEntry(item).then(() => this.fetchItems());
+    }
+
+    changeItem(key, data) {
+        removeEntry(key).then(() => this.addItem(data)).then(() => this.fetchItems());
+    }
+
+    addItem(item) {
+        return addEntry(item.url, item.id, item.key).then(() => this.fetchItems());
     }
 
     render() {
@@ -21,17 +40,22 @@ export class OptionsList extends React.Component {
         let listItems = [];
 
         for (let key in items) {
-            listItems.push(<OptionsListItem key={key} accessKey={key} url={items[key]["url"]}/>)
+            listItems.push(<OptionsListItem
+                removeItem={this.removeItem}
+                changeItem={this.changeItem}
+                key={key}
+                accessKey={key}
+                selector={items[key]["id"]}
+                url={items[key]["url"]}/>)
         }
-
-        console.info(`${listItems.length} items to add`);
 
         return (<table className="table table-striped" id="entries-table">
             <thead>
             <tr>
                 <th>Keyword</th>
                 <th>Url</th>
-                <th>Delete</th>
+                <th>Selector</th>
+                <th>Action</th>
             </tr>
             </thead>
             <tbody id="entries-table-body">{listItems}</tbody>
@@ -46,20 +70,36 @@ export class OptionsListItem extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = this.initialState();
         this.onRemoveClick = this.onRemoveClick.bind(this);
+        this.onChangeClick = this.onChangeClick.bind(this);
+    }
+
+    initialState() {
+        return  {
+            id: this.props.selector,
+            key: this.props.accessKey,
+            url: this.props.url
+        };
     }
 
     onRemoveClick() {
-        console.debug(`remove ${this.props.accessKey}|${this.props.url}`);
-        removeEntry(this.props.accessKey);
+        this.props.removeItem(this.state.key);
+    }
+
+    onChangeClick() {
+        this.props.changeItem(this.props.accessKey, this.state);
     }
 
     render() {
+
         return <tr>
-            <td>{this.props.accessKey}</td>
-            <td>{this.props.url}</td>
+            <td><input type="text" name="key" value={this.state.key} onChange={(e) => this.setState({key:e.target.value})}/></td>
+            <td><input type="text" name="url" value={this.state.url} onChange={(e) => this.setState({url:e.target.value})}/></td>
+            <td><input type="text" name="selector" value={this.props.selector} disabled/></td>
             <td>
-                <button className="btn btn-danger btn-xs" onClick={this.onRemoveClick}>x</button>
+                <button className="btn btn-success btn-xs" onClick={this.onChangeClick}>update</button>&nbsp;
+                <button className="btn btn-danger btn-xs" onClick={this.onRemoveClick}>delete</button>
             </td>
         </tr>;
     }
@@ -67,7 +107,11 @@ export class OptionsListItem extends React.Component {
     static propTypes = {
         accessKey: PropTypes.string,
         url: PropTypes.string,
+        selector: PropTypes.string,
+        removeItem: PropTypes.func,
+        changeItem: PropTypes.func
     }
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
